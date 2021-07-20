7037f4675a8c7ecd37db542e836a110b8c9b0975
AzeezFazry <azeezfazry@gmail.com>
2021-07-20 18:57:04 +0530

    Login page and activity pages are implemented.

---
+++ app/src/main/java/com/azeezfazry/studentmanagementsystem/MainActivity.java
@@ -0,0 +1,48 @@
+package com.azeezfazry.studentmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buLoginAction(View view) {
+        EditText editTextUsername = findViewById(R.id.etUsername);
+        EditText editTextPassword = findViewById(R.id.etPassword);
+        String username = editTextUsername.getText().toString();
+        String password = editTextPassword.getText().toString();
+        if (isValidLogin(username,password)) {
+            Intent intent = new Intent(MainActivity.this, OptionsActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * Function to validate the user
+     * @param username
+     * @param password
+     * @return
+     */
+    private boolean isValidLogin(String username, String password) {
+        if(username == "admin") {
+            if(password == "admin@123") {
+                return true;
+            } else {
+                Toast.makeText(this, "Incorrect Password", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Incorrect Username", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+}
